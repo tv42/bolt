e927eff317cb2f8640cca95129a1faabb754b496
Tommi Virtanen <tv@eagain.net>
2015-02-18 11:45:40 -0800

    Validate batch benchmark results

--- batch_benchmark_test.go
+++ batch_benchmark_test.go
@@ -1,13 +1,50 @@
 package bolt_test
 
 import (
+	"bytes"
 	"encoding/binary"
+	"errors"
 	"hash/fnv"
 	"sync"
 	"testing"
 
 	"github.com/boltdb/bolt"
 )
+
+func validateBatchBench(b *testing.B, db *TestDB) {
+	var rollback = errors.New("sentinel error to cause rollback")
+	validate := func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte("bench"))
+		h := fnv.New32a()
+		buf := make([]byte, 4)
+		for id := uint32(0); id < 1000; id++ {
+			binary.LittleEndian.PutUint32(buf, id)
+			h.Reset()
+			h.Write(buf[:])
+			k := h.Sum(nil)
+			v := bucket.Get(k)
+			if v == nil {
+				b.Errorf("not found id=%d key=%x", id, k)
+				continue
+			}
+			if g, e := v, []byte("filler"); !bytes.Equal(g, e) {
+				b.Errorf("bad value for id=%d key=%x: %s != %q", id, k, g, e)
+			}
+			if err := bucket.Delete(k); err != nil {
+				return err
+			}
+		}
+		// should be empty now
+		c := bucket.Cursor()
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			b.Errorf("unexpected key: %x = %q", k, v)
+		}
+		return rollback
+	}
+	if err := db.Update(validate); err != nil && err != rollback {
+		b.Error(err)
+	}
+}
 
 func BenchmarkDBBatchAutomatic(b *testing.B) {
 	db := NewTestDB()
@@ -44,6 +81,9 @@ func BenchmarkDBBatchAutomatic(b *testing.B) {
 		close(start)
 		wg.Wait()
 	}
+
+	b.StopTimer()
+	validateBatchBench(b, db)
 }
 
 func BenchmarkDBBatchSingle(b *testing.B) {
@@ -80,6 +120,9 @@ func BenchmarkDBBatchSingle(b *testing.B) {
 		close(start)
 		wg.Wait()
 	}
+
+	b.StopTimer()
+	validateBatchBench(b, db)
 }
 
 func BenchmarkDBBatchManual10x100(b *testing.B) {
@@ -121,4 +164,7 @@ func BenchmarkDBBatchManual10x100(b *testing.B) {
 		close(start)
 		wg.Wait()
 	}
+
+	b.StopTimer()
+	validateBatchBench(b, db)
 }
